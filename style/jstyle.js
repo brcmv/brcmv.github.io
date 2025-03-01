2e6e52fc9a1ec47a6268cc08be55c6f1b07fcfd6
BOBE ROWYN CHRIS <brcmvallejos@mymail.mapua.edu.ph>
2025-03-01 22:39:38 +0800

    carousel style unfinished

---
+++ style/jstyle.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var navbar = $(".navbar");
+    var brand = $(".navbar-brand");
+    var jumbotronHeight = $(".homeHeader").outerHeight();
+    var navbarHeight = navbar.outerHeight();
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= jumbotronHeight) {
+            navbar.addClass("sticky-nav");
+            brand.addClass("visible-brand");
+            $("body").css("padding-top", navbarHeight + "px"); // Prevent snapping
+        } else {
+            navbar.removeClass("sticky-nav");
+            brand.removeClass("visible-brand");
+            $("body").css("padding-top", "0");
+        }
+    });
+});
